ee8dd97d07476365ee15986e41a66f5dd174ce07
Weliton Souza <welitonfbs@gmail.com>
2018-04-27 13:41:43 -0300

    Update RECUPERACAO.java

--- RECUPERACAO.java
+++ RECUPERACAO.java
@@ -1,1 +1,22 @@
+//(A) NÚMERO PERFEITO
+import java.util.Scanner;
 
+public class Main {
+  public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+    
+    int entrada = in.nextInt();
+    int valor = in.nextInt();
+    
+    for (int i=0; i < entrada; i++) {
+      valor = in.nextInt();
+        if (valor % 2 == 0) {
+          System.out.println(valor+" eh perfeito");
+        } else if (valor % 2 != 0) {
+          System.out.println(valor+"nao eh perfeito");
+        }
+      valor = in.nextInt();
+    }    
+    in.close();  
+  }
+}
